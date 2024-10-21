2a82a3c9929d924dac6963c2fcf1ec0064fe6dbc
blackPie8 <stanashray@gmail.com>
2024-10-21 13:45:47 +0530

    version

---
+++ subStr.java
@@ -0,0 +1,19 @@
+// import java.util.*;
+
+// class subStr{
+//     public static void main(String[] args) {
+//         String str  = "abcdsc";
+//         String currSubStr = "abcd";
+//         int start=0;
+//         int end=1;
+
+//         for(int i=0;i<str.length();i++){
+//             if(currSubStr.indexOf(str.charAt((start))==-1)){
+//                 currSubStr += str.charAt(start);
+//             }
+//             else{
+//                 if(la)
+//             }
+//         }
+//     }
+// }
